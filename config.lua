50a3be71edec0ae7d6ad85a6e3fe7acd34514d67
TropicGalxy <104592582+TropicGalxy@users.noreply.github.com>
2025-04-10 17:13:52 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,7 +7,19 @@ Config.ThirstColor = "#38bdf8"
 Config.OxygenColor = "#67e8f9"
 Config.SpeedColor = "#ff4d4d"
 
-Config.FuelExport = Entity(veh).state.fuel
+Config.FuelSystem = "ox"
+
+Config.GetFuel = function(veh)
+    if Config.FuelSystem == "cdn-fuel" then
+        return exports["cdn-fuel"]:GetFuel(veh)
+    elseif Config.FuelSystem == "legacy-fuel" then
+        return exports["LegacyFuel"]:GetFuel(veh)
+    elseif Config.FuelSystem == "ox" then
+        return Entity(veh).state.fuel
+    else
+        print("Invalid fuel script")
+    end
+end
 
 Config.Speed = 2.23694 -- 2.23694 for mph, 3.6 for kmh
 
